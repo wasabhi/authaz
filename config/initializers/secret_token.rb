44c8e2fff56986c37df5e4d660c629a4437d2393
wasabhi <abhinav.keswani@gmail.com>
2011-05-07 14:25:51 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authaz::Application.config.secret_token = 'e92d626a923ff0749d4557eae430c69bd4d4427f99827a928b2b2299e9cf871939a7bd1193358e7c42a4f73937577f395fd6ba5e37be5b6fe14888e4addb7500'
